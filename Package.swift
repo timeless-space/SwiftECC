8bb6b47c96eeea2b133a020fac29fd5bcab35998
Jatin <jatin.k@simformsolutions.com>
2022-12-27 15:40:44 +0530

    Remove depedency.

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftECC",
-            dependencies: ["ASN1", "BigInt"]),
+            dependencies: []),
         .testTarget(
             name: "SwiftECCTests",
             dependencies: ["SwiftECC"]),
